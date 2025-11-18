8f2d557cddf5f159875f0f25523335565037d943
Stanley Seow <stanleyseow@users.noreply.github.com>
2025-11-18 13:41:25 +0800

    Add files via upload

---
+++ sampleGame2/game.js
@@ -0,0 +1,22 @@
+﻿var config = {
+    type: Phaser.AUTO,
+    // pixel size * tile map size * zoom 
+    width: 32 * 20,
+    height: 32 * 20,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false,
+            debug: true
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    backgroundColor: '#ff0000',
+    pixelArt: true,
+    scene: [preload, world, room1]
+};
+
+var game = new Phaser.Game(config);
